387aba64609db3cb84110ee48b7536c2cbe2177f
Porfirio Perez <prezgonzlez.por@gmail.com>
2018-03-04 15:24:42 -0300

    Usuarios: sql

---
+++ usuario.sql
@@ -0,0 +1,14 @@
+INSERT INTO PUBLIC.USUARIO (id_usuario, nombre, apellido, nombre_usuario, contrasenha, rol, grupo) 
+VALUES (1, pablo, aguilar, paguilar, cambiar123, 1, 1);
+INSERT INTO PUBLIC.USUARIO (id_usuario, nombre, apellido, nombre_usuario, contrasenha, rol, grupo) 
+VALUES (2,jacqueline, gill, jgill, cambiar123, 2, 1);
+INSERT INTO PUBLIC.USUARIO (id_usuario, nombre, apellido, nombre_usuario, contrasenha, rol, grupo) 
+VALUES (3, oscar, vega, ovega, cambiar123, 3, 1);
+INSERT INTO PUBLIC.USUARIO (id_usuario, nombre, apellido, nombre_usuario, contrasenha, rol, grupo) 
+VALUES (4, porfirio, perez, pperez, cambiar123, 5, 1);
+INSERT INTO PUBLIC.USUARIO (id_usuario, nombre, apellido, nombre_usuario, contrasenha, rol, grupo) 
+VALUES (5, lilian, riveros, lriveros, cambiar123, 4, 1);
+INSERT INTO PUBLIC.USUARIO (id_usuario, nombre, apellido, nombre_usuario, contrasenha, rol, grupo) 
+VALUES (6, lilian, demattei, ldemattei, cambiar123, 4, 1);
+INSERT INTO PUBLIC.USUARIO (id_usuario, nombre, apellido, nombre_usuario, contrasenha, rol, grupo) 
+VALUES (7, sebastian, fernandez, sfernandez, cambiar123, 4, 1);
