5929b92bdb88acc10e9517f218a2e55297e419ca
Jacqueline Gill <jgilloli@gmail.com>
2018-03-04 14:53:18 -0300

    Estados del proyecto

---
+++ estado_proyecto.sql
@@ -0,0 +1,5 @@
+INSERT INTO estado_proyecto VALUES(1,'Vacio');
+INSERT INTO estado_proyecto VALUES(2,'Planificada');
+INSERT INTO estado_proyecto VALUES(3,'En Proceso');
+INSERT INTO estado_proyecto VALUES(4,'Hecho');
+INSERT INTO estado_proyecto VALUES(5,'Descartada');
